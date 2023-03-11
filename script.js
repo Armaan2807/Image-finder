50b87abf62da0b6f543c84e13fa053f57252c752
mohit2314 <mohitpatel2314@gmail.com>
2023-03-11 12:11:32 +0530

    completed project

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
 let imgUrl = "placeholder-img.png";
-let searchKeyword =""
+let searchKeyword = "";
 function inputSearchKeyWord(event) {
-searchKeyword =event.target.value;
+  searchKeyword = event.target.value;
 }
-async function searchImages (){
-    document.getElementById("loader").style.visibility="visible"
- imgUrl= await fetch(`https://api.unsplash.com/search/photos?page=1&query=${searchKeyword}&client_id=tn9DHhsB9W4InO0FKiehqtaeOESz81yS_m6USoISP6Q`)
- const response = await imgUrl.json();
- document.getElementById("loader").style.visibility="hidden"
- console.log(document.getElementsByClassName("next-cards"))
- let nextCards=document.getElementsByClassName("next-cards")
- nextCards.forEach(card=>{
-    card.style.visibility="visible"
-    })
- console.log(response)
- if (response.results.length==0) {
-const toastLiveExample = document.getElementById('liveToast')
-const toast = new bootstrap.Toast(toastLiveExample)
-toast.show()
- }
- imgUrl = response.results[0].urls.regular
- document.getElementById("photo-id").src=imgUrl;
- 
+async function searchImages() {
+  document.getElementById("loader").style.visibility = "visible";
+  imgUrl = await fetch(
+    `https://api.unsplash.com/search/photos?page=1&query=${searchKeyword}&client_id=tn9DHhsB9W4InO0FKiehqtaeOESz81yS_m6USoISP6Q`
+  );
+  const response = await imgUrl.json();
+  document.getElementById("loader").style.visibility = "hidden";
+  let nextCards = document.getElementsByClassName("next-cards");
+
+  for (let i = 0; i < nextCards.length; i++) {
+    nextCards[i].style.visibility = "visible";
+  }
+  console.log(response);
+  if (response.results.length == 0) {
+    const toastLiveExample = document.getElementById("liveToast");
+    const toast = new bootstrap.Toast(toastLiveExample);
+    toast.show();
+  }
+
+  //Assigning urls from the responsr to the card images
+  let allCards = document.getElementsByClassName("card-img-top");
+  for (let i = 0; i < allCards.length; i++) {
+    allCards[i].src = response.results[i].urls.regular;
+  }
+  imgUrl = response.results[0].urls.regular;
+  document.getElementById("photo-id").src = imgUrl;
 }
